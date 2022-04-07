6925adecd0244dc4b4ef3b110409678fabbf3a7d
leana-faye <102371490+leana-faye@users.noreply.github.com>
2022-04-07 17:40:37 -0400

    Update months.py

--- months.py
+++ months.py
@@ -19,6 +19,30 @@ def monthString(monthNum):
      ### this is the only section    ###
      ### you change in this program. ###
      ###################################
+if monthNum == 1:
+     return("January")
+elif monthNum == 2:
+     return("February")
+elif monthNum == 3:
+     return("March")
+elif monthNum == 4:
+     return("April")
+elif monthNum == 5:
+     return("May")
+elif monthNum == 6:
+     return("June")
+elif monthNum == 7:
+     return("July")
+elif monthNum == 8:
+     return("August")
+elif monthNum == 9:
+     return("September")
+elif monthNum == 10:
+     return("October")
+elif monthNum == 11:
+     return("November")
+elif monthNum == 12:
+     return("December")
 
      return(monthString)
 
